e798f265c3014486f4de5a81ded52850eb0e891e
Robert Burger <Burger@bogen.local>
2016-06-07 15:04:37 +0200

    First commit of stable version. Currently v0.1.0. will be changed to version v1.0.0

---
+++ __openerp__.py
@@ -0,0 +1,10 @@
+{
+  'name':'Redo The App v0.1.0',
+  'description': 'Looks like I have to redo the app because I did not save all of my data locally and reguarly',
+  'version':'0.1.0',
+  'author':'Rob Burgertech',
+
+  'data': ['views/layout.xml', 'views/pages.xml'],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
